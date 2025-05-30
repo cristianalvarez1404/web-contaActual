0820129a3237cec1cd75d78d76a4e5f644261253
cristian <cristianalvarez1496@gmail.com>
2025-05-30 11:21:12 -0500

    adding form to newpost

--- client/src/pages/newPost/NewPost.tsx
+++ client/src/pages/newPost/NewPost.tsx
@@ -1,10 +1,77 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NewPost = () => {
+  
+  const [message,setMessage] = useState("")
+  const [error,setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleFormPost = async (e:any) => {
+    e.preventDefault()
+    const form = new FormData(e.target)
+    
+    const newArticle:any = {}
+
+    if (!form.get('title') || !form.get('description') || !form.get('date')) {
+      setError("Todos los campos obligatorios deben estar llenos");
+      setTimeout(() => {
+        setError("")
+      },5000)
+      return;
+    }
+
+    for(const [key,value] of form.entries()){
+      (key === "category") 
+      ?
+        newArticle[key] = Number(value)
+      :
+        newArticle[key] = value
+    }
+
+    try {
+      const req = await axios.post('http://localhost:3000/',newArticle)
+      if(req.status >= 200 && req.status < 300){
+        setMessage(`Article created succefully 🎉🥳`)
+        setTimeout(() => {
+          setMessage("")
+          navigate("/")
+        },4000)
+      }else {
+        throw new Error(`Error creating article ${newArticle.title} - Error - ${req.status}`)
+      }
+
+    }catch(err){
+      console.log(err)
+      if(err instanceof Error) {
+        setError(err.message)
+        setTimeout(() => {
+          setError("")
+        },5000)
+      }
+      else if(typeof err === "string" ){
+        setError(err)
+        setTimeout(() => {
+          setError("")
+        },5000)
+      }else {
+        setError("Error in request 😢")
+        setTimeout(() => {
+          setError("")
+        },5000)
+      }
+    }
+  }
+
+
+
   return (
     <div className="min-h-[calc(100vh-100px)] w-[85%] m-auto">
+      {error && <p className="w-fit p-2 mt-5 m-auto rounded-2xl bg-red-500 text-white">{error}</p> }
+      {message && <p className="w-fit p-2 mt-5 m-auto rounded-2xl bg-green-700 text-white">{message}</p>}
       <h2 className="text-3xl mt-5 mb-5">Crear post</h2>
-      <form action="">
+      <form onSubmit={handleFormPost}>
         <div className="mb-5">
           <label
             htmlFor=""
@@ -15,6 +82,7 @@ const NewPost = () => {
           <input
             type="text"
             placeholder="title"
+            name="title"
             className="w-[90%] border border-gray-400 outline-none rounded border-gray-50 p-1"
           />
         </div>
@@ -28,6 +96,7 @@ const NewPost = () => {
           <textarea
             className="w-full block border-2 border-gray-400 rounded-2xl outline-none p-5"
             rows={10}
+            name="description"
             placeholder="Write your content"
           />
         </div>
@@ -51,12 +120,12 @@ const NewPost = () => {
           >
             Tipo
           </label>
-          <select name="" id="">
-            <option value="tributaria">Tributaria</option>
-            <option value="nomina">Nómina</option>
-            <option value="tributaria">Contabilidad</option>
-            <option value="tributaria">Costos</option>
-            <option value="tributaria">Otros</option>
+          <select name="category" id="">
+            <option value="1">Tributaria</option>
+            <option value="2">Nómina</option>
+            <option value="3">Contabilidad</option>
+            <option value="21">Costos</option>
+            <option value="1">Otros</option>
           </select>
         </div>
         <div className="mb-5">
@@ -66,7 +135,8 @@ const NewPost = () => {
           >
             Fecha del post
           </label>
-          <span>{new Date().toLocaleDateString()}</span>
+          <input className="ml-2" type="date" name="date"/>
+          {/* <span>{new Date().toLocaleDateString()}</span> */}
         </div>
         <div className="mb-10 flex items-center justify-center">
           <input
@@ -75,6 +145,7 @@ const NewPost = () => {
             className="w-[10rem] bg-transparent border-2 border-blue-900 cursor-pointer text-blue-900 px-5 py-2 rounded-2xl font-bold hover:bg-blue-900 hover:text-white hover:transition-colors"
           />
         </div>
+        
       </form>
     </div>
   );
