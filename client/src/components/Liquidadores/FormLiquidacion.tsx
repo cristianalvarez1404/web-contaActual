0f02b674c69bad99bdc390d1a5a6db56701c7afc
CristianAlvarez <cristianandres1496@gmail.com>
2025-06-09 14:32:29 -0500

    creating store in zustand

--- client/src/components/Liquidadores/FormLiquidacion.tsx
+++ client/src/components/Liquidadores/FormLiquidacion.tsx
@@ -1,25 +1,110 @@
-import React from "react";
-import { useStore } from "../../store";
+import React, { useEffect } from "react";
+import { useLiquidacion, useStore } from "../../store";
 
 const FormLiquidacion = () => {
-  const dias = useStore((state: any) => state.diasTotales);
-  const calcularDiasTotales = useStore((state: any) => state.totalDias);
-  const calcularTotalDiasCesantias = useStore(
-    (state: any) => state.totalDiasCesantias
+  const fechaInicial = useStore((state: any) => state.fechaInicial);
+  const fechaFinal = useStore((state: any) => state.fechaFinal);
+  const salario = useStore((state: any) => state.salario);
+  const auxilioTransporte = useStore((state: any) => state.auxilioTransporte);
+  const vacacionesDisfrutadas = useStore(
+    (state: any) => state.vacacionesDisfrutadas
   );
-  const calcularTotalDiasIntereses = useStore(
-    (state: any) => state.totalDiasIntereses
+  const dias = useStore((state: any) => state.totalDias);
+  const diasPrestaciones = useLiquidacion(
+    (state: any) => state.totalDiasPrestaciones
   );
-  const calcularTotalDiasPrima = useStore((state: any) => state.totalDiasPrima);
-  const calcularTotalDiasVacaciones = useStore(
+  const diasVacaciones = useLiquidacion(
     (state: any) => state.totalDiasVacaciones
   );
+  const valorCesantias = useLiquidacion(
+    (state: any) => state.totalValorCesantias
+  );
+  const valorIntereses = useLiquidacion(
+    (state: any) => state.totalValorIntereses
+  );
+  const valorPrima = useLiquidacion(
+    (state: any) => state.totalValorPrimaServicios
+  );
+  const valorVacaciones = useLiquidacion(
+    (state: any) => state.totalValorVacaciones
+  );
+  const valorLiquidacion = useLiquidacion(
+    (state: any) => state.totalLiquidacion
+  );
 
-  console.log(calcularDiasTotales);
+  const setFechaInial = useStore((state: any) => state.setFechaInicial);
+  const setFechaFinal = useStore((state: any) => state.setFechaFinal);
+  const setCalculoDiasTotales = useStore((state: any) => state.setDiasTotales);
+  const setVacacionesDisfrutadas = useStore(
+    (state: any) => state.setVacacionesDisfrutadas
+  );
+  const setSalario = useStore((state: any) => state.setSalario);
+  const setAuxilioTransporte = useStore(
+    (state: any) => state.setAuxilioTransporte
+  );
+
+  const setTotalDiasPrestaciones = useLiquidacion(
+    (state: any) => state.setTotalDiasPrestaciones
+  );
+  const setTotalDiasVacaciones = useLiquidacion(
+    (state: any) => state.setTotalDiasVacaciones
+  );
+  const setTotalCesantias = useLiquidacion(
+    (state: any) => state.setTotalCesantias
+  );
+  const setTotalIntereses = useLiquidacion(
+    (state: any) => state.setTotalIntereses
+  );
+  const setTotalPrima = useLiquidacion((state: any) => state.setTotalPrima);
+
+  const setTotalVacaciones = useLiquidacion(
+    (state: any) => state.setTotalVacaciones
+  );
+
+  const setTotalLiquidacion = useLiquidacion(
+    (state: any) => state.setTotalLiquidacion
+  );
+
+  useEffect(() => {
+    if (fechaInicial && fechaFinal) {
+      setCalculoDiasTotales();
+      setTotalDiasPrestaciones(fechaInicial, fechaFinal);
+      setTotalDiasVacaciones(fechaInicial, fechaFinal);
+      setTotalCesantias(salario, auxilioTransporte);
+      setTotalIntereses();
+      setTotalPrima(salario, auxilioTransporte);
+      setTotalVacaciones({
+        salario,
+        vacacionesDisfrutadas: vacacionesDisfrutadas || 0,
+      });
+      setTotalLiquidacion();
+    }
+  }, [fechaInicial, fechaFinal]);
+
+  useEffect(() => {
+    console.log(salario);
+    console.log("Días para prestaciones:", diasPrestaciones);
+    console.log("Días para vacaciones:", diasVacaciones);
+    console.log("Total para cesantias:", valorCesantias);
+    console.log("Total para interes:", valorIntereses);
+    console.log("Total para prima:", valorPrima);
+    console.log("Total para vacaciones:", valorVacaciones);
+  }, [diasPrestaciones]);
+
+  const convertToPeso = (valor: any) => {
+    const formatoCOP = new Intl.NumberFormat("es-CO", {
+      style: "currency",
+      currency: "COP",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(valor);
+    return formatoCOP;
+  };
+
   return (
     <div className="w-[90%] min-h-[50vh] m-auto p-5">
       <h2 className="text-xl text-blue-800 font-bold mb-5">
-        Liquidador de contrato laboral {dias}
+        Liquidador de contrato laboral
       </h2>
       <form className="flex gap-5">
         <div className="flex-1">
@@ -42,6 +127,7 @@ const FormLiquidacion = () => {
               type="text"
               id="cedula"
               className="border border-gray-400 p-2 rounded-xl"
+              onClick={setCalculoDiasTotales}
             />
           </div>
           <div className="p-2 flex flex-col">
@@ -69,6 +155,7 @@ const FormLiquidacion = () => {
               type="date"
               id=""
               className="border border-gray-400 p-2 rounded-xl"
+              onChange={(e) => setFechaInial(e.target.value)}
             />
           </div>
           <div className="p-2 flex flex-col">
@@ -79,6 +166,7 @@ const FormLiquidacion = () => {
               type="date"
               id=""
               className="border border-gray-400 p-2 rounded-xl"
+              onChange={(e) => setFechaFinal(e.target.value)}
             />
           </div>
           <div className="p-2 flex flex-col">
@@ -90,10 +178,9 @@ const FormLiquidacion = () => {
               id=""
               min={0}
               defaultValue={0}
+              value={vacacionesDisfrutadas}
               className="border border-gray-400 p-2 rounded-xl"
-              onChange={(e) =>
-                calcularTotalDiasVacaciones(Number(e.target.value))
-              }
+              onChange={(e) => setVacacionesDisfrutadas(e.target.value)}
             />
           </div>
           <div className="p-2 flex flex-col">
@@ -105,7 +192,9 @@ const FormLiquidacion = () => {
               id=""
               min={0}
               defaultValue={0}
+              value={salario}
               className="border border-gray-400 p-2 rounded-xl"
+              onChange={(e) => setSalario(e.target.value)}
             />
           </div>
           <div className="p-2 flex flex-col">
@@ -117,7 +206,9 @@ const FormLiquidacion = () => {
               id=""
               min={0}
               defaultValue={0}
+              value={auxilioTransporte}
               className="border border-gray-400 p-2 rounded-xl"
+              onChange={(e) => setAuxilioTransporte(e.target.value)}
             />
           </div>
         </div>
@@ -126,52 +217,54 @@ const FormLiquidacion = () => {
           <div className="flex flex-col gap-5 border border-gray-400 p-5 rounded-2xl">
             <div className="border-b-2 border-gray-300 border-dashed p-2">
               <h3 className="font-medium">Total días laborados:</h3>
-              <p>210 días</p>
+              <p>{fechaInicial && fechaFinal ? dias : 0}</p>
             </div>
             <div className="flex items-center border-b-2 border-gray-300 border-dashed p-2">
               <div className="flex-1">
                 <h3 className="font-medium">Total días Cesantias:</h3>
-                <p>180 días</p>
+                <p>{diasPrestaciones && diasPrestaciones} días</p>
               </div>
               <div className="mt-2 flex-1">
                 <h3 className="font-medium">Valor cesantias:</h3>
-                <p>$5.000.000</p>
+                <p>{valorCesantias ? convertToPeso(valorCesantias) : 0}</p>
               </div>
             </div>
             <div className="flex items-center border-b-2 border-gray-300 border-dashed p-2">
               <div className="flex-1">
                 <h3 className="font-medium">Total días Intereses Cesantias:</h3>
-                <p>180 días</p>
+                <p>{diasPrestaciones && diasPrestaciones} días</p>
               </div>
               <div className="mt-2 flex-1">
                 <h3 className="font-medium">Valor Intereses cesantias:</h3>
-                <p>$800.000</p>
+                <p>{valorIntereses ? convertToPeso(valorIntereses) : 0}</p>
               </div>
             </div>
             <div className="flex items-center border-b-2 border-gray-300 border-dashed p-2">
               <div className="flex-1">
                 <h3 className="font-medium">Total días prima de servicios:</h3>
-                <p>60 días</p>
+                <p>{diasPrestaciones && diasPrestaciones} días</p>
               </div>
               <div className="mt-2 flex-1">
                 <h3 className="font-medium">Valor prima de servicios:</h3>
-                <p>$910.000</p>
+                <p>{valorPrima ? convertToPeso(valorPrima) : 0}</p>
               </div>
             </div>
             <div className="flex items-center border-b-2 border-gray-300 border-dashed p-2">
               <div className="flex-1">
                 <h3 className="font-medium">Total días vacaciones:</h3>
-                <p>60 días</p>
+                <p>{diasVacaciones && diasVacaciones} días</p>
               </div>
               <div className="mt-2 flex-1">
                 <h3 className="font-medium">Valor vacaciones:</h3>
-                <p>$480.000</p>
+                <p>{valorVacaciones ? convertToPeso(valorVacaciones) : 0}</p>
               </div>
             </div>
             <div className="flex items-center border-b-2 border-gray-300 border-dashed p-2">
               <div className="mt-2 flex items-center gap-2">
                 <h3 className="font-medium text-3xl">Total liquidación:</h3>
-                <p className="text-xl font-thin">$17.000.000</p>
+                <p className="text-xl font-thin">
+                  {convertToPeso(valorLiquidacion)}
+                </p>
               </div>
             </div>
           </div>
--- client/src/store.ts
+++ client/src/store.ts
@@ -1,14 +1,125 @@
 import { create } from "zustand";
 
 export const useStore = create((set) => ({
-  diasTotales: 0,
-  cesantias: 0,
-  intereses: 0,
-  prima: 0,
-  totalDias: (dias: any) => set((state: any) => ({ diasTotales: dias })),
-  totalDiasCesantias: (dias: any) => set((state: any) => ({ cesantias: dias })),
-  totalDiasIntereses: (dias: any) => set((state: any) => ({ intereses: dias })),
-  totalDiasPrima: (dias: any) => set((state: any) => ({ prima: dias })),
-  totalDiasVacaciones: (dias: any) =>
-    set((state: any) => ({ vacaciones: dias })),
+  diasVacacionesDisfrutados: 0,
+  salario: 0,
+  auxilioTransporte: 0,
+  fechaInicial: 0,
+  fechaFinal: 0,
+  totalDias: 0,
+  setVacacionesDisfrutadas: (dias: any) =>
+    set(() => ({ diasVacacionesDisfrutados: Number(dias) })),
+
+  setSalario: (valorSalario: any) =>
+    set(() => ({ salario: Number(valorSalario) })),
+
+  setAuxilioTransporte: (valorAuxilio: any) =>
+    set(() => ({ auxilioTransporte: Number(valorAuxilio) })),
+
+  setFechaInicial: (fecha: any) =>
+    set(() => ({ fechaInicial: new Date(fecha) })),
+
+  setFechaFinal: (fecha: any) => set(() => ({ fechaFinal: new Date(fecha) })),
+
+  setDiasTotales: () =>
+    set((state: any) => {
+      console.log(state);
+      const diferencia =
+        state.fechaFinal && state.fechaInicial
+          ? state.fechaFinal.getTime() - state.fechaInicial.getTime()
+          : 0;
+      const dias = Math.ceil(diferencia / (1000 * 60 * 60 * 24)) + 1;
+      return {
+        totalDias: dias,
+      };
+    }),
+}));
+
+export const useLiquidacion = create((set) => ({
+  totalDiasPrestaciones: 0,
+  totalDiasVacaciones: 0,
+  totalValorCesantias: 0,
+  totalValorIntereses: 0,
+  totalValorPrimaServicios: 0,
+  totalValorVacaciones: 0,
+  totalLiquidacion: 0,
+
+  setTotalDiasPrestaciones: (fechaInicial: any, fechaFinal: any) =>
+    set(() => {
+      if (!fechaInicial || !fechaFinal) return { totalDiasPrestaciones: 0 };
+
+      const añoActual = new Date().getFullYear();
+      const añoInicial = fechaInicial.getFullYear();
+      const añoFinal = fechaFinal.getFullYear();
+      const fechaInicialCalculo = new Date(añoActual, 0, 1);
+      let diasPrestaciones = 0;
+
+      if (añoFinal === añoActual && añoInicial < añoActual) {
+        diasPrestaciones = fechaFinal.getTime() - fechaInicialCalculo.getTime();
+      } else if (añoFinal === añoActual && añoInicial === añoActual) {
+        diasPrestaciones = fechaFinal.getTime() - fechaInicial.getTime();
+      }
+
+      diasPrestaciones = Math.ceil(
+        diasPrestaciones / (1000 * 60 * 60 * 24) + 1
+      );
+      return { totalDiasPrestaciones: diasPrestaciones };
+    }),
+
+  setTotalDiasVacaciones: (fechaInicial: any, fechaFinal: any) =>
+    set(() => {
+      const ms = fechaFinal.getTime() - fechaInicial.getTime();
+      const diasTrabajados = Math.ceil(ms / (1000 * 60 * 60 * 24));
+      const diasVacaciones = ((diasTrabajados / 360) * 15).toFixed(2);
+      return { totalDiasVacaciones: diasVacaciones };
+    }),
+
+  setTotalCesantias: (salario: any, auxilio: any) =>
+    set((state: any) => {
+      if (!salario) return { totalValorCesantias: 0 };
+      return {
+        totalValorCesantias: Math.round(
+          ((salario + auxilio) * state.totalDiasPrestaciones) / 360
+        ),
+      };
+    }),
+
+  setTotalIntereses: () =>
+    set((state: any) => {
+      return {
+        totalValorIntereses: Math.round(
+          (state.totalValorCesantias * 0.12 * state.totalDiasPrestaciones) / 360
+        ),
+      };
+    }),
+
+  setTotalPrima: (salario: any, auxilio: any) =>
+    set((state: any) => {
+      if (!salario) return { totalValorPrimaServicios: 0 };
+      return {
+        totalValorPrimaServicios: Math.round(
+          ((salario + auxilio) * state.totalDiasPrestaciones) / 360
+        ),
+      };
+    }),
+
+  setTotalVacaciones: (info: any) =>
+    set((state: any) => {
+      // if (!info.salario) return { totalValorVacaciones: 0 };
+      return {
+        totalValorVacaciones: Math.round(
+          (info.salario * state.totalDiasVacaciones) / 30 -
+            info.vacacionesDisfrutadas
+        ),
+      };
+    }),
+
+  setTotalLiquidacion: () =>
+    set((state: any) => ({
+      totalLiquidacion:
+        state.totalValorCesantias +
+        state.totalValorIntereses +
+        state.totalValorPrimaServicios +
+        state.totalValorVacaciones,
+    })),
 }));
